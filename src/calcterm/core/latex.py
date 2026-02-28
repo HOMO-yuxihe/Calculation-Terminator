2a20c47248aac655892d0836ba778fa704367b3e
HOMO-yuxihe <2459379510@qq.com>
2026-02-28 22:14:14 +0800

    v0.4-pre2:修复了预览表达式时对有些部分多乘1的问题

--- src/calcterm/core/latex.py
+++ src/calcterm/core/latex.py
@@ -1,4 +1,5 @@
 import sympy
+from sympy.core.numbers import One
 import matplotlib.pyplot as plt
 from io import BytesIO
 
@@ -6,6 +7,26 @@ plt.rcParams['text.usetex']=False
 plt.rcParams['mathtext.fontset']='cm'
 plt.rcParams['text.latex.preamble']=r'\usepackage{bm}'  # 或 \usepackage{amsmath}
 plt.rcParams['svg.fonttype']='path'
+
+def remove_mul_1(node:sympy.Expr):
+    if not node.args:
+            return node
+    new_args=[]
+    if isinstance(node,sympy.Mul):
+        for arg in node.args:
+            processed_arg=remove_mul_1(arg)
+            if not isinstance(processed_arg,One):
+                new_args.append(processed_arg)
+    else:
+        new_args=[remove_mul_1(arg) for arg in node.args]
+    if isinstance(node,sympy.Mul) and len(new_args)==1:
+        return new_args[0]
+    else:
+        try:
+            return node.func(*new_args,evaluate=False)
+        except ValueError:
+            return node.func(*new_args)
+
 def latex2svg(tex:str,font_size=12):
     if not tex.strip():return ''
     text=f'${tex}$'
@@ -28,14 +49,14 @@ def latex2svg(tex:str,font_size=12):
 
 def expr2latex(expr:str):
     if not expr.strip():return ''
-    expression=sympy.parse_expr(expr,evaluate=False)
+    expression=remove_mul_1(sympy.parse_expr(expr,evaluate=False))
     return sympy.latex(expression,mode='plain')
 
 if __name__ == '__main__':
     pass
     # text = r'$x^2+y^2=z^2$'
     # print(latex_formula2svg(text,font_size=12))
-    expr='x**2+y**2-z**2'
+    expr='2*4*6'
     tex=expr2latex(expr)
     print(tex)
-    print(latex2svg(tex))
+    # print(latex2svg(tex))
